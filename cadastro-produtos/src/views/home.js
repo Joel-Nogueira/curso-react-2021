95371db349aeb61cff6457350571fb9aeeb77158
Joel-Nogueira <joelnogueira733@gmail.com>
2021-02-27 19:56:20 -0300

    Incluídos o componente funcional navbar e a view Home

---
+++ cadastro-produtos/src/views/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div class="jumbotron">
+            <h1>Bem vindo!</h1>
+            <p>Utilize a barra de navegação para acessar as páginas</p>
+            <p><a class="btn btn-primary btn-lg">Cadastrar</a></p>
+        </div>  
+    );
+}
+
+export default Home;
